b3ead1945ae76af823afd8a64d69698d8f9d0cef
Dom Porada <dom@dom.engineering>
2026-01-14 12:00:18 +0100

    Update `prettier-plugin-sort-json`
    Gudahtt/prettier-plugin-sort-json#292

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-/* oxlint-disable-next-line import/no-unassigned-import */
-import './types/index.ts';
-
 export { default as defineConfig } from './define-config/index.ts';
 export { default as prioritizeKeys } from './prioritize-keys/index.ts';
